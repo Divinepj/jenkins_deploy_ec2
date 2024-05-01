9ffdd98747c7ca9f808c1af73e06a3a021d07e42
Adebayo Victor <adebayovolanrewaju@yahoo.com>
2024-05-01 19:58:02 -0400

    new

---
+++ shellscript.sh
@@ -0,0 +1,46 @@
+# name="John"
+
+#!/bin/bash
+
+# Example script to check if a number is positive, negative, or zero
+
+# read -p "Enter a number: " num
+
+# if [ $num -gt 0 ]; then
+#     echo "The number is positive."
+# elif [ $num -lt 0 ]; then
+#     echo "The number is negative."
+# else
+#     echo "The number is zero."
+# fi
+
+#!/bin/bash
+
+# Example script to print numbers from 1 to 5 using a for loop
+
+# for (( i=1; i<=5; i++ ))
+# do
+#     echo $i
+# done
+
+# echo "Enter your name:"
+# read name
+
+# echo "Hello World"
+# echo "hello world" >> index.txt
+
+# grep "pattern" < index.txt
+# echo "hello world" | grep "pattern"
+
+
+#!/bin/bash
+
+# Define a function to greet the user
+greet() {
+    echo "Hello, $1! Nice to meet you."
+}
+
+# Call the greet function and pass the name as an argument
+greet "John"
+
+
