eebe460f4f53da5d44dff660d7a5b76b9bc5f117
Adebayo Victor <adebayovolanrewaju@yahoo.com>
2024-05-01 23:23:28 -0400

    updasss

--- shellscript.sh
+++ shellscript.sh
@@ -1,19 +1,34 @@
-{
-  "actions": [
-    {
-      "name": "Check Apache Status",
-      "type": "command",
-      "command": "systemctl status apache2"
-    },
-    {
-      "name": "Create Folder",
-      "type": "command",
-      "command": "mkdir kevin"
-    },
-    {
-      "name": "Create Text File",
-      "type": "command",
-      "command": "touch kevin/webhook.txt"
+pipeline {
+    agent any
+
+    stages {
+        stage('Execute Actions') {
+            steps {
+                script {
+                    def actions = [
+                        [
+                            name: "Check Apache Status",
+                            type: "command",
+                            command: "systemctl status apache2"
+                        ],
+                        [
+                            name: "Create Folder",
+                            type: "command",
+                            command: "mkdir kevin"
+                        ],
+                        [
+                            name: "Create Text File",
+                            type: "command",
+                            command: "touch kevin/webhook.txt"
+                        ]
+                    ]
+
+                    for (action in actions) {
+                        echo "Executing action: ${action.name}"
+                        sh "${action.command}"
+                    }
+                }
+            }
+        }
     }
-  ]
 }
