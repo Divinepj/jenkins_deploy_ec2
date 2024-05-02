c421281fe87f637969d9fa0b6219c7405938c90d
Adebayo Victor <adebayovolanrewaju@yahoo.com>
2024-05-01 23:28:17 -0400

    kkk

--- shellscript.sh
+++ shellscript.sh
@@ -7,11 +7,6 @@ pipeline {
                 script {
                     def actions = [
                         [
-                            name: "Check Apache Status",
-                            type: "command",
-                            command: "systemctl status apache2"
-                        ],
-                        [
                             name: "Create Folder",
                             type: "command",
                             command: "mkdir kevin"
